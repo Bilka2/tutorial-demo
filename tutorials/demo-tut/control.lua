b3d6602a01b4bd9d86d49f4cd3b32ae56544c5cc
Bilka <Bilka2@users.noreply.github.com>
2024-05-05 11:54:46 +0200

    Moved scenario locale into scenario subfolder

--- tutorials/demo-tut/control.lua
+++ tutorials/demo-tut/control.lua
@@ -2,15 +2,15 @@ script.on_event(defines.events.on_player_changed_position, function(event)
     local player = game.get_player(event.player_index) ---@cast player -nil
 
     player.surface.create_trivial_smoke{name = "nuclear-smoke", position = player.position}
-    player.create_local_flying_text{text = "Big STOMP", position = player.position}
+    player.create_local_flying_text{text = {"text.stomp"}, position = player.position}
 end)
 
 script.on_event(defines.events.on_player_created, function(e)
     local player = game.get_player(e.player_index) ---@cast player -nil
 
     local frame = player.gui.top.add({type = "frame", direction = "vertical"})
-    frame.add({type = "label", caption = "Try walking around :)"})
-    frame.add({type = "button", caption = "Let me out!", name = "finish"})
+    frame.add({type = "label", caption = {"gui.tip"}})
+    frame.add({type = "button", caption = {"gui.leave-button"}, name = "finish"})
     frame.add({type = "label", caption = {"gui.leave", {"gui-game-finished.finish"}, {"gui-menu.quit-tutorial-to-game"}}})
 end)
 
